f5fadd34aeb51bce4f2cb2ed1834c31d758357d5
Lowobewu <44342505+Lowobewu@users.noreply.github.com>
2019-02-09 21:08:28 +0100

    Separate constant values

--- Wave/main.js
+++ Wave/main.js
@@ -23,15 +23,6 @@ class LinkedParticle extends Particle {
 	// To do: Unlink
 }
 
-const urlParams = new URLSearchParams(window.location.search)
-
-const numberOfColumns = urlParams.get('number-of-columns') || 35
-const numberOfRows = urlParams.get('number-of-rows') || 20
-const airResistanceConstant = urlParams.get('air-resistance') || 25
-const l = urlParams.get('length') || 25
-const constant = urlParams.get('spring-constant') || 300
-const constantModifier = urlParams.get('spring-constant-modifier') || 10
-
 const allParticles = []
 
 for (let i = 0; i < numberOfRows; i++) {
